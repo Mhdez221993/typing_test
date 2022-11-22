3d848f54d5fe886a65b337060a8f311b7ecbba9e
Moises <mhdezcoronado@gmail.com>
2022-11-22 17:58:32 +0100

    Import components into app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import './App.css';
+import TestArea from "./Input";
+import Result from "./Result";
+import { setFinished, updataTimer } from "./testSlice";
+import Text from "./Text";
 
 function App() {
+  const {started, timer, finished} = useSelector(state => state.test)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if(!started) return
+
+    const interval = setInterval(() => {
+      if (timer === 0) {
+        dispatch(setFinished(true))
+        return
+      }
+
+      dispatch(updataTimer())
+
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [started, timer, dispatch])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Speed Typing Test</h1>
+
+        {
+          !finished &&
+          <div>
+            <div className="timer"><p>{timer}</p></div>
+            <Text />
+            <TestArea />
+          </div>
+        }
+
+        {
+          finished &&
+          <Result />
+        }
     </div>
   );
 }
